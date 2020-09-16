8a700262f2ebfac2d110213535e7f91335424d50
Adib Istanbouli <adib.istanbouli@gmail.com>
2020-09-16 19:37:39 +0200

    API typing

--- src/services/API.d.ts
+++ src/services/API.d.ts
@@ -2,5 +2,9 @@ declare namespace API {
   export interface QuestionnaireData {
     id: string;
     name?: string;
+    questions?: Object[{
+      required: boolean;
+    }];
+    description?: string;
   }
 }
